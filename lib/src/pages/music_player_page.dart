143860727e018b522abb51574ff8c27aff6e2cd7
Javier Claros <javiclaros@gmail.com>
2023-03-27 12:23:08 -0400

    agregué barra de progreso

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -9,8 +9,144 @@ class MusicPlayerPage extends StatelessWidget {
       body: Column(
         children: const [
           CustomAppbar(),
+
+          ImagenDiscoDuracion(),
           // Text('Hola Mundo'),
         ],
+      ),
+    );
+  }
+}
+
+class ImagenDiscoDuracion extends StatelessWidget {
+  const ImagenDiscoDuracion({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      margin: const EdgeInsets.only(top: 70),
+      child: Row(
+        children: [
+          // TODO: Disco
+          const ImagenDisco(),
+          SizedBox(
+            width: size.width * 0.08,
+          ),
+          // TODO: Barra de progreso
+          const BarraProgreso(),
+          SizedBox(
+            width: size.width * 0.05,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BarraProgreso extends StatelessWidget {
+  const BarraProgreso({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final estilo = TextStyle(color: Colors.white.withOpacity(0.4));
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            '00:00',
+            style: estilo,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Stack(
+            children: [
+              Container(
+                width: 3,
+                height: 230,
+                color: Colors.white.withOpacity(0.1),
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  width: 3,
+                  height: 100,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            '00:00',
+            style: estilo,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ImagenDisco extends StatelessWidget {
+  const ImagenDisco({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(200),
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          // end: Alignment.centerRight,
+          colors: [
+            Color(0xff484750),
+            Color(0xff1E1C24),
+          ],
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(200),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            const Image(
+              image: AssetImage('assets/aurora.jpg'),
+            ),
+            Container(
+              width: 25,
+              height: 25,
+              decoration: const BoxDecoration(
+                color: Colors.black38,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(100),
+                ),
+              ),
+            ),
+            Container(
+              width: 18,
+              height: 18,
+              decoration: const BoxDecoration(
+                color: Color(0xff1C1C25),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(100),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
